d052e77c1b77514843eaa0e63211fde3d621b420
nedhub <54211897+nedhub@users.noreply.github.com>
2021-07-25 17:53:11 -0400

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -4,6 +4,7 @@ import boto3
 # We can use the low-level client to make API calls to DynamoDB.
 client = boto3.client('dynamodb', region_name='us-east-2')
 
+
 try:
     resp = client.create_table(
         TableName="Books",
